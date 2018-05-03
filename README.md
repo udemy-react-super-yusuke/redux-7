2195e17c63b9b211d1fafb33bb6406ad47654054
udemy-react-super-yusuke <38956215+udemy-react-super-yusuke@users.noreply.github.com>
2018-05-04 01:39:59 +0900

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# redux-7
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+import { Provider } from "react-redux";
+
+// containers のほうを使う
+import App from "./containers/App";
+
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
